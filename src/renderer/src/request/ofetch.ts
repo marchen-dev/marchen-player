874635af2526f4178205996e54799ce1681143a5
suemor233 <suemor233@outlook.com>
2025-01-27 22:48:20 +0800

    feat: add request error message

--- src/renderer/src/request/ofetch.ts
+++ src/renderer/src/request/ofetch.ts
@@ -1,9 +1,35 @@
+import { toast } from '@renderer/components/ui/toast'
 import { API_URL } from '@renderer/lib/env'
 import { ofetch } from 'ofetch'
 
 const apiFetch = ofetch.create({
   baseURL: API_URL,
   timeout: 10000,
+  onResponseError: (error) => {
+    switch (error.response.status) {
+      case 403: {
+        toast({
+          title: '403 Forbidden',
+          description: `接口请求被拒绝, 请联系开发者更新相关秘钥 - ${error.response.url}`,
+        })
+        break
+      }
+      default: {
+        toast({
+          title: '接口请求失败',
+          description: error.request.toString(),
+        })
+      }
+    }
+  },
+  // onResponse: (response) => {
+  //   const responseData = response.response._data
+  //   if (responseData?.success === false) {
+  //     toast({
+  //       description: responseData?.errorMessage,
+  //     })
+  //   }
+  // },
 })
 
 export const Get = <T = object>(url: string, params?: object): Promise<T> =>
